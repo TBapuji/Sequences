3cf7ac440203a4dabc3eaf53503187dd356a7f6a
Tony Bapuji <tony_bapuji@yahoo.co.uk>
2021-09-19 22:21:13 +0100

    Comparing performance of int array and List<int>

--- Sequence/PrimeNumber.cs
+++ Sequence/PrimeNumber.cs
@@ -8,7 +8,7 @@ namespace Sequence
 {
     public class PrimeNumber
     {
-        public static int[] Retrieve(int numberOfPrimeNumbers)
+        public static int[] RetrieveWithArray(int numberOfPrimeNumbers)
         {
             int[] primeNumbers = new int[numberOfPrimeNumbers];
             primeNumbers[0] = 2;
@@ -33,6 +33,29 @@ namespace Sequence
                     j += 1;
                     primeNumbers[j] = i;
                 }               
+            }
+            return primeNumbers;
+        }
+
+        public static List<int> RetrieveWithList(int numberOfPrimeNumbers)
+        {
+            List<int> primeNumbers = new List<int>();
+            primeNumbers.Add(2);
+            int i = 1;
+            bool assignPM = false;
+            while(true)
+            {
+                i++;
+                foreach(int primeNumber in primeNumbers)
+                {
+                    assignPM = true;
+                    if(i % primeNumber == 0) { assignPM = false; break; }
+                }
+                if(assignPM)
+                {
+                    primeNumbers.Add(i);
+                    if(primeNumbers.Count == numberOfPrimeNumbers) break;
+                }
             }
             return primeNumbers;
         }
--- Sequence/Program.cs
+++ Sequence/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Sequence
 {
@@ -6,8 +7,43 @@ namespace Sequence
     {
         static void Main(string[] args)
         {
-        foreach(int primeNumber in PrimeNumber.Retrieve(30))
-            Console.WriteLine(primeNumber);
+            int numberOfPrimeNumbers = 30000;
+            
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Console.WriteLine($"RetrieveWithArray {PrimeNumber.RetrieveWithArray(numberOfPrimeNumbers).Length}");
+            //foreach(int primeNumber in PrimeNumber.RetrieveWithList(numberOfPrimeNumbers))
+            //{ Console.WriteLine(primeNumber); }
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.Elapsed.TotalSeconds.ToString());
+
+
+            Stopwatch stopWatch2 = new Stopwatch();
+            stopWatch2.Start();
+            Console.WriteLine($"RetrieveWithList, Number {PrimeNumber.RetrieveWithList(numberOfPrimeNumbers).Count}");
+
+            //foreach(int primeNumber in PrimeNumber.RetrieveWithArray(numberOfPrimeNumbers))
+            //{ Console.WriteLine(primeNumber); }
+            stopWatch2.Stop();
+            Console.WriteLine(stopWatch2.Elapsed.TotalSeconds.ToString());
+
+            Stopwatch stopWatch3 = new Stopwatch();
+            stopWatch3.Start();
+            Console.WriteLine($"RetrieveWithArray {PrimeNumber.RetrieveWithArray(numberOfPrimeNumbers).Length}");
+            //foreach(int primeNumber in PrimeNumber.RetrieveWithList(numberOfPrimeNumbers))
+            //{ Console.WriteLine(primeNumber); }
+            stopWatch3.Stop();
+            Console.WriteLine(stopWatch3.Elapsed.TotalSeconds.ToString());
+
+
+            Stopwatch stopWatch4 = new Stopwatch();
+            stopWatch4.Start();
+            Console.WriteLine($"RetrieveWithList, Number {PrimeNumber.RetrieveWithList(numberOfPrimeNumbers).Count}");
+
+            //foreach(int primeNumber in PrimeNumber.RetrieveWithArray(numberOfPrimeNumbers))
+            //{ Console.WriteLine(primeNumber); }
+            stopWatch4.Stop();
+            Console.WriteLine(stopWatch4.Elapsed.TotalSeconds.ToString());
         }
     }
 }
