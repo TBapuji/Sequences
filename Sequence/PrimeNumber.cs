b642552d488cb6b295e3138a983b21c7cc8d048c
Tony Bapuji <tony_bapuji@yahoo.co.uk>
2021-09-19 08:38:45 +0100

    Add project files.

---
+++ Sequence/PrimeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sequence
+{
+    public class PrimeNumber
+    {
+        public static List<int> Retrieve(int numberOfPrimeNumbers)
+        {
+            List<int> primeNumbers = new List<int>();
+            primeNumbers.Add(2);
+            int i = 1;
+            bool assignPM = false;
+            while(true)
+            {
+                i++;
+                foreach(int primeNumber in primeNumbers)
+                {
+                    assignPM = true;
+                    if(i % primeNumber == 0) { assignPM = false; break; }
+                }
+                if(assignPM) 
+                { 
+                    primeNumbers.Add(i);
+                    if(primeNumbers.Count == numberOfPrimeNumbers) break;
+                }
+            }
+            return primeNumbers;
+        }
+    }
+}
---
+++ Sequence/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        foreach(int primeNumber in PrimeNumber.Retrieve(1229))
+            Console.WriteLine(primeNumber);
+        }
+    }
+}
